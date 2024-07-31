388b298531c5f27c6b780ef4eae0a896a16527be
Jeff Jansen <jeff@claypotfrog.com>
2024-07-30 21:31:03 -0400

    version 2.3 files

---
+++ database/18.sql
@@ -0,0 +1,3 @@
+update settings set value = '1' where name = 'show_at_startup'
+insert into settings(name,value) values('system_appearance','auto')
+insert into settings(name,value) values('auto_rotate','1')
