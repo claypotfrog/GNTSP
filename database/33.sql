37e2871036300b364ec12405379f1746608ae6f5
Jeff Jansen <jeff@claypotfrog.com>
2025-06-09 19:59:56 -0400

    version 2.9 files

---
+++ database/33.sql
@@ -0,0 +1,13 @@
+update entries set parsetxt = 'adverb (comparative)' where wordID = 106968
+update dictionary set gloss = 'a greedy person' where lemma = 'πλεονέκτης'
+update dictionary set gloss = 'truly, indeed<cr>μέν ... δέ - on the one hand ... on the other hand' where lemma = 'μέν'
+update dictionary set gloss = 'a bowl' where lemma = 'τρύβλιον'
+update dictionary set gloss = 'a small dish' where lemma = 'παροψίς'
+update dictionary set gloss = 'I assign by lot<cr>προσκληρόομαι - I join, associate with' where lemma = 'προσκληρόω'
+update dictionary set gloss = 'I am greatly annoyed' where lemma = 'διαπονέομαι'
+
+update verses set verse = 8.1 where num = 2 and chapter = 16 and verse = 21
+update lebverses set verse = 8.1 where num = 2 and chapter = 16 and verse = 21
+update versehilites set verse = 8.1 where verse = '8b'
+update bookmarks set verse = 8.1 where verse = '8b'
+update versehilites set color = "#F9966B" where color = "LightSalmon"
